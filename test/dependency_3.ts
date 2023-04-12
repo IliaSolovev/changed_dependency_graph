05e69349b2886f1578f187c9aea77030ee200121
Ilia_Solovev1 <49729985+IliaSolovev@users.noreply.github.com>
2023-04-12 11:54:07 +0300

    feat: added tests files

---
+++ test/dependency_3.ts
@@ -0,0 +1,1 @@
+export const dependency_3 = 3
--- test/test_file.ts
+++ test/test_file.ts
@@ -1,3 +1,5 @@
 import {dependency_1} from './dependency_1';
+import {dependency_2} from './dependency_2';
+import {dependency_3} from './dependency_3';
 
-const workWithDependency = dependency_1 + 1
+const workWithDependency = dependency_1 + dependency_2 + dependency_3
