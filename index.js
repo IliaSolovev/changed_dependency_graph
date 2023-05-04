4e48522e08532e9074de221b79970a8f73e3dfed
Ilia_Solovev1 <49729985+IliaSolovev@users.noreply.github.com>
2023-05-04 19:14:39 +0300

    feat: remove ts files

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const {Octokit} = require("@octokit/rest");
-const {getChangedFileDependencies} = require('./lib/getChangedFileDependencies');
+const {getChangedFileDependencies} = require('./lib/getChangedFileDependencies.js');
 const { exec } = require("child_process");
 const octokit = new Octokit();
 // https://github.com/IliaSolovev/changed_dependency_graph/pull/1
@@ -8,7 +8,7 @@ octokit.rest.pulls.listFiles({
     owner: 'IliaSolovev',
     repo: 'changed_dependency_graph',
     pull_number: '2',
-} as any).then(({data}) => {
+} ).then(({data}) => {
     const command = `madge --extensions ts --image graph.svg ${Array.from(getChangedFileDependencies(data)).join(' ')}`
     console.log(command)
     exec(command, (error, stdout, stderr) => {
